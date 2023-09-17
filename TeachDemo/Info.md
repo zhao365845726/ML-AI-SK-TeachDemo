68dd837bd6728a8081d95f902dad377b7fc8f09d
MLTechnology <zhao365845726@163.com>
2023-09-17 23:13:28 +0800

    初始化代码

---
+++ TeachDemo/Info.md
@@ -0,0 +1,23 @@
+https://learningprompt.wiki/
+
+https://learningprompt.wiki/docs
+
+https://learn.microsoft.com/en-us/semantic-kernel/whatissk
+
+https://azure.microsoft.com/en-us/products/
+
+
+## 案例
+
+Semantic Kernel Github 仓库
+https://github.com/microsoft/semantic-kernel 
+
+Semantic Kernel Microsoft Learn 文档
+https://learn.microsoft.com/en-us/semantic-kernel/
+
+Semantic Kernel 团队博客
+https://devblogs.microsoft.com/semantic-kernel/
+https://devblogs.microsoft.com/semantic-kernel/newsletter/?nm=confirmation&nk=348-93da378f00d22970dabab6672349479f
+
+John Meada 的SK Recipes
+https://github.com/johnmaeda/SK-Recipes
---
+++ TeachDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Text;
+//using Microsoft.SemanticKernel;
+////using Microsoft.SemanticKernel.KernelExtensions;
+//using Microsoft.SemanticKernel.Orchestration;
+////using MyOpenAI.Shared;
+
+//初始化Kernel
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Orchestration;
+using System.Text;
+
+var myKernel = Kernel.Builder.Build();
+myKernel.Config.AddAzureOpenAITextCompletionService(
+    "davinci-azure",
+    "text-davinci-003",
+    "{your azure openai endpoint}",
+    "{your azure openai key}");
+//导入技能    
+var mySkill = myKernel.ImportSemanticSkillFromDirectory("Skills", "Learning");
+var myContext = new ContextVariables();
+StringBuilder histories = new StringBuilder();
+Console.WriteLine("Say anything to start practicing English.");
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    var input = Console.ReadLine();
+//填充变量
+    myContext.Set("history", histories.ToString());
+    myContext.Set("input", input);
+//运行技能
+    var myResult = await myKernel.RunAsync(myContext, mySkill["LearningEnglishSkill"]);
+    histories.AppendLine(input);
+    histories.AppendLine(myResult.Result.ToString());
+    Console.WriteLine(myResult);
+}
